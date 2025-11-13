e722b44d52ef630e8ed9ec3d4cd06975079fbd2a
ryanstewart047 <78749841+ryanstewart047@users.noreply.github.com>
2025-11-13 15:10:25 +0000

    Fix Font Awesome: revert to simple CSS link

--- app/layout.tsx
+++ app/layout.tsx
@@ -155,26 +155,10 @@ export default function RootLayout({
           strategy="lazyOnload"
         />
         
-        {/* Font Awesome - Deferred loading */}
-        <link 
-          rel="preload"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-          as="style"
-        />
+        {/* Font Awesome */}
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-          media="print"
-        />
-        <Script
-          id="load-font-awesome"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              const fontAwesome = document.querySelector('link[href*="font-awesome"]');
-              if (fontAwesome) fontAwesome.media = 'all';
-            `
-          }}
         />
         
         {/* PWA Meta Tags - Updated */}
